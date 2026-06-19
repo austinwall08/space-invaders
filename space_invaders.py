265ec6299a506b3956ef1b040f066e4fb07aa7a1
Austin Wall <austin.wal08@gmail.com>
2026-06-19 03:32:41 -0500

    finished Product

--- space_invaders.py
+++ space_invaders.py
@@ -8,9 +8,6 @@ window = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
 
-# left to do *******
-# collide bullet with enemy method, add fuel system, plus point system, start screen?, start background
-
 # region of variables
 global start_pos_x
 global start_pos_y
@@ -42,6 +39,20 @@ player_surface = pygame.Surface((player_surface_x, player_surface_y), pygame.SRC
 
 global angle
 angle = 0
+
+global fuel
+fuel = 100
+
+global accel_x
+accel_x = 0
+global accel_y
+accel_y = 0
+
+global game_begin
+game_begin = False
+
+global score
+score = 0
 # endregion
 
 def draw_player():
@@ -162,17 +173,21 @@ def boundary(player_rect):
 def inertia():
     global velocity_x, velocity_y
     global start_pos_x, start_pos_y
-    accel_x = 0
-    accel_y = 0
+    global fuel
+    global accel_x, accel_y
     acceleration = 100
 
-    if press_keys[pygame.K_w]:
-        accel_x -= math.sin(math.radians(angle)) * acceleration
-        accel_y -= math.cos(math.radians(angle)) * acceleration
+    accel_x = 0
+    accel_y = 0
+ 
+    if fuel > 0:
+        if press_keys[pygame.K_w]:
+            accel_x -= math.sin(math.radians(angle)) * acceleration
+            accel_y -= math.cos(math.radians(angle)) * acceleration
 
-    if press_keys[pygame.K_s]:
-        accel_x += math.sin(math.radians(angle)) * acceleration
-        accel_y += math.cos(math.radians(angle)) * acceleration 
+        if press_keys[pygame.K_s]:
+            accel_x += math.sin(math.radians(angle)) * acceleration
+            accel_y += math.cos(math.radians(angle)) * acceleration 
 
     velocity_x += accel_x * dt
     velocity_y += accel_y * dt
@@ -182,7 +197,7 @@ def inertia():
     draw_player()
 
 def reset_game():
-    global start_pos_x, start_pos_y, angle, velocity_x, velocity_y, spawn_time
+    global start_pos_x, start_pos_y, angle, velocity_x, velocity_y, spawn_time, fuel, score
 
     enemies.clear()
     bullets.clear()
@@ -192,6 +207,67 @@ def reset_game():
     velocity_x = 0
     velocity_y = 0
     spawn_time = 0
+    fuel = 100
+    score = 0
+
+def fuel_system():
+    global fuel, accel_x, accel_y
+
+    if fuel <= 0:
+        fuel = 0
+        fuel_out_font = pygame.font.SysFont("stencil", 35)
+        fuel_out_text_surface = fuel_out_font.render("FUEL IS OUT, SHOOT ASTEROIDS TO GET MORE", True, ("Red"))
+        window.blit(fuel_out_text_surface, (1280/4.5, 720/2))
+    elif accel_x != 0 or accel_y != 0:
+        fuel -= 10 * dt
+    fuel_font = pygame.font.SysFont("segoeuisemilight", 35)
+    fuel_text_surface = fuel_font.render(f"Fuel: {int(fuel)}", True, (255, 255, 255))
+    window.blit(fuel_text_surface, (1100, 620))
+
+    return fuel
+
+def start_screen():
+
+    start_font = pygame.font.SysFont("corbel", 80)
+    control_font = pygame.font.SysFont("corbel", 25)
+    control_display_font = pygame.font.SysFont("corbel", 50)
+    start_game_font = pygame.font.SysFont("corbel", 15)
+
+    start_screen = pygame.Surface((1280, 720))
+    start_screen.fill("White")
+
+    line1 = start_font.render("Welcome to Spenis Invaders", True, (255, 255, 255))
+    line2 = control_display_font.render("Controls:", True, (255, 255, 255))
+    line3 = control_font.render("WASD to move", True, (255, 255, 255))
+    line4 = control_font.render("Space to Shoot", True, (255, 255, 255))
+    line5 = control_font.render("ESC to Pause", True, (255, 255, 255))
+    line6 = control_font.render("Good Luck!", True, (255, 255, 255))
+    line7 = start_game_font.render("(G to Start)", True, (255, 255, 255))
+
+    window.blit(line1, (225, 100))
+    window.blit(line2, (550, 200))
+    window.blit(line3, (575, 280))
+    window.blit(line4, (575, 360))
+    window.blit(line5, (580, 440))
+    window.blit(line6, (600, 540))
+    window.blit(line7, (620, 600))
+
+def display_score():
+    global score
+
+    score_font = pygame.font.SysFont("Corbel", 50)
+    score_display = score_font.render(f"Score: {int(score)}", True, (255, 255, 255))
+    window.blit(score_display, (400, 25))
+
+def display_max_score():
+    global score
+
+    scores.append(score)
+
+    max_score = max(scores)
+    max_score_font = pygame.font.SysFont("Corbel", 50)
+    max_score_display = max_score_font.render(f"Max Score: {int(max_score)}", True, (255, 255, 255))
+    window.blit(max_score_display, (600, 25))
 
 class Enemy:
 
@@ -257,8 +333,13 @@ class Enemy:
         else: return False
 
     def take_damage(self, bullet):
+        global fuel
+        global score
+
         self.durability -= bullet.damage
         if self.durability <= 0:
+            fuel += self.radius
+            score += self.radius
             enemies.remove(enemy)
 
 class Bullet:
@@ -293,9 +374,20 @@ class Bullet:
 
     def leave_boundary(self):
         if self.x >= 1282.5 or self.x <= -2.5:
-            bullets.remove(self)
+            if bullet in bullets:
+                bullets.remove(self)
         if self.y >= 722.5 or self.y <= -2.5:
-            bullets.remove(self)
+            if bullet in bullets:
+                bullets.remove(self)
+
+class Stars:
+    def __init__(self, x, y,):
+        self.x = x
+        self.y = y
+        self.radius = 2
+
+    def draw_stars(self):
+        pygame.draw.circle(window, "white", (self.x, self.y), self.radius)
 
 global game_paused
 game_paused = False
@@ -303,9 +395,10 @@ game_paused = False
 global game_lost
 game_lost = False
 
+scores = [0]
 enemies = []
 bullets = []
-
+stars = []
 
 global spawn_time
 spawn_time = 0
@@ -321,91 +414,108 @@ while running is True:
             elif event.key == pygame.K_SPACE and not game_paused and len(bullets) < 10:
                 bullets.append(Bullet(start_pos_x, player_rect.top, angle))
 
+            if event.key == pygame.K_g:
+                game_begin = True
+
     dt = clock.tick(60) / 1000
+    window.fill("black")
 
-    if game_lost == False:
+    if game_begin == False:
+        start_screen()
 
-        if game_paused == False:
-            
-            random_score_amount = random.randrange(5, 21)
+    if game_begin == True:
+        if len(stars) < 50:
+            stars.append(Stars(random.randrange(0, 1280), random.randrange(0, 720)))
 
-            spawn_time += dt
+        for star in stars:
+            star.draw_stars()
 
-            if len(enemies) < 20 and spawn_time > .5:
-                enemies.append(Enemy(random.randrange(40, 1280), random.randrange(-75, 805, 820), random_score_amount, random_score_amount, random_score_amount))
-                spawn_time = 0
+        if game_lost == False:
 
-            window.fill("black")
+            if game_paused == False:
 
-            press_keys = pygame.key.get_pressed()
+                random_score_amount = random.randrange(5, 21)
 
-            rotated_surface = rotate_player()
+                spawn_time += dt
 
-            inertia()
+                if len(enemies) < 20 and spawn_time > .5:
+                    enemies.append(Enemy(random.randrange(40, 1280), random.randrange(-75, 805, 820), random_score_amount, random_score_amount, random_score_amount))
+                    spawn_time = 0
 
-            player_rect = rotated_surface.get_rect(center=(start_pos_x, start_pos_y))
-            window.blit(rotated_surface, player_rect)
-            
-            boundary(player_rect)
+                display_score()
 
-            for bullet in bullets:
-                bullet.move_bullet()
-                bullet.spawn_bullet()
-                bullet.leave_boundary()
+                display_max_score()
+
+                press_keys = pygame.key.get_pressed()
+
+                rotated_surface = rotate_player()
+
+                inertia()
+
+                player_rect = rotated_surface.get_rect(center=(start_pos_x, start_pos_y))
+                window.blit(rotated_surface, player_rect)
+                
+                boundary(player_rect)
+
+                for bullet in bullets:
+                    bullet.move_bullet()
+                    bullet.spawn_bullet()
+                    bullet.leave_boundary()
+                    for enemy in enemies:
+                        if bullet.collide_with_enemy(enemy) == True:
+                            enemy.take_damage(bullet)
+                            if bullet in bullets:
+                                bullets.remove(bullet)
+                                
                 for enemy in enemies:
-                    if bullet.collide_with_enemy(enemy) == True:
-                        enemy.take_damage(bullet)
-                        bullets.remove(bullet)
-                            
-            for enemy in enemies:
-                enemy.move_enemy()
-                enemy.boundary()
-                enemy.spawn_enemy()
-                if enemy.colliding_with_player() == True:
-                    game_lost = True
+                    enemy.move_enemy()
+                    enemy.boundary()
+                    enemy.spawn_enemy()
+                    if enemy.colliding_with_player() == True:
+                        game_lost = True
+                
+                for self in range(len(enemies)):
+                    for enemy in range(self + 1, len(enemies)):
+                        enemies[self].enemy_collision(enemies[enemy])
+
+                fuel_system()
+
+            elif game_paused == True:          
+                pause_menu = pygame.Surface((1280, 720), pygame.SRCALPHA)
+                grey_trans = (100, 100, 100, 5)
+                pause_menu.fill(grey_trans)
+
+                pause_font = pygame.font.SysFont("algerian", 100)
+                pause_text_surface = pause_font.render("Game Paused", True, (255, 255, 255))
+                pause_text_rect = pause_text_surface.get_rect(center = (1280/2, 720/2))
+
+                pause_menu.blit(pause_text_surface, pause_text_rect)
+                window.blit(pause_menu, (0, 0))
+
+        elif game_lost == True:
+            game_over = pygame.Surface((1280, 720))
+            game_over.fill("red")
             
-            for self in range(len(enemies)):
-                for enemy in range(self + 1, len(enemies)):
-                    enemies[self].enemy_collision(enemies[enemy])
-            
-            pygame.display.flip()
+            scores.append(score)
 
-        elif game_paused == True:          
-            pause_menu = pygame.Surface((1280, 720), pygame.SRCALPHA)
-            grey_trans = (100, 100, 100, 5)
-            pause_menu.fill(grey_trans)
+            font = pygame.font.SysFont("algerian", 100)
+            font2 = pygame.font.SysFont("algerian", 30)
 
-            pause_font = pygame.font.SysFont("algerian", 100)
-            pause_text_surface = pause_font.render("Game Paused", True, (255, 255, 255))
-            pause_text_rect = pause_text_surface.get_rect(center = (1280/2, 720/2))
+            text_surface = font.render("Game Over", True, (255, 255, 255))
+            text_surface2 = font2.render("Press Enter To Play Again", True, (255, 255, 255))
 
-            pause_menu.blit(pause_text_surface, pause_text_rect)
-            window.blit(pause_menu, (0, 0))
+            text_rect = text_surface.get_rect(center = (1280/2, 720/2))
+            text_rect2 = text_surface2.get_rect(center = (625, 470))
 
-            pygame.display.flip()
+            game_over.blit(text_surface2, text_rect2)
+            game_over.blit(text_surface, text_rect)
+            window.blit(game_over, (0, 0))
 
-    elif game_lost == True:
-        game_over = pygame.Surface((1280, 720))
-        game_over.fill("red")
-        
+            if pygame.key.get_pressed()[pygame.K_RETURN]:
+                game_lost = False
+                reset_game()
 
-        font = pygame.font.SysFont("algerian", 100)
-        font2 = pygame.font.SysFont("algerian", 30)
-
-        text_surface = font.render("Game Over", True, (255, 255, 255))
-        text_surface2 = font2.render("Press Enter To Play Again", True, (255, 255, 255))
-
-        text_rect = text_surface.get_rect(center = (1280/2, 720/2))
-        text_rect2 = text_surface2.get_rect(center = (625, 470))
-
-        game_over.blit(text_surface2, text_rect2)
-        game_over.blit(text_surface, text_rect)
-        window.blit(game_over, (0, 0))
-
-        if pygame.key.get_pressed()[pygame.K_RETURN]:
-            game_lost = False
-            reset_game()
-        pygame.display.flip()
+    pygame.display.flip()
     
 pygame.quit()
 
